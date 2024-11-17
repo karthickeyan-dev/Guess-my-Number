e27d257020c1f280d97574d805b320920e03c47a
karthickeyan <jkarthickeyan24@gmail.com>
2024-11-17 12:35:01 +0530

    Implemented the game logic

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 "use strict";
 
-document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let scoreNumber = 20;
 
-  if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No Number!";
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guessNumber = Number(document.querySelector(".guess").value);
+
+  if (scoreNumber > 1) {
+    if (!guessNumber) {
+      document.querySelector(".message").textContent = "⛔ No Number!";
+    } else if (guessNumber === secretNumber) {
+      document.querySelector(".message").textContent = "🎉 Correct Number!";
+    } else if (guessNumber > secretNumber) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      document.querySelector(".score").textContent = --scoreNumber;
+    } else if (guessNumber < secretNumber) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      document.querySelector(".score").textContent = --scoreNumber;
+    }
+  } else {
+    document.querySelector(".message").textContent = "💥 You lost the game";
+    document.querySelector(".score").textContent = 0;
   }
 });
