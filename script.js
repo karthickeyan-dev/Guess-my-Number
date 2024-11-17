7848a08388639c294e0f5b3b7d26a5493cac969d
karthickeyan <jkarthickeyan24@gmail.com>
2024-11-17 14:29:33 +0530

    Added reset function

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let scoreNumber = 20;
-
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guessNumber = Number(document.querySelector(".guess").value);
@@ -13,6 +11,9 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "⛔ No Number!";
     } else if (guessNumber === secretNumber) {
       document.querySelector(".message").textContent = "🎉 Correct Number!";
+      document.querySelector(".number").textContent = secretNumber;
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".number").style.width = "30rem";
     } else if (guessNumber > secretNumber) {
       document.querySelector(".message").textContent = "📈 Too high!";
       document.querySelector(".score").textContent = --scoreNumber;
@@ -24,4 +25,15 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "💥 You lost the game";
     document.querySelector(".score").textContent = 0;
   }
+});
+
+document.querySelector(".reset").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  scoreNumber = 20;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = scoreNumber;
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
